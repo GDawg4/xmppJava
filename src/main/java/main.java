61b9d0187a1ec80e898849b9314dc115723a9788
Rodrigo Garoz <35437197+GDawg4@users.noreply.github.com>
2021-08-10 23:34:37 -0600

    Less basic stuff

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,11 +1,16 @@
 
-import org.jivesoftware.smack.XMPPConnection;
-import org.jivesoftware.smack.Chat;
-import org.jivesoftware.smack.ChatManager;
-import org.jivesoftware.smack.ChatManagerListener;
-import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.*;
 import org.jivesoftware.smack.packet.Message;
 import org.jetbrains.annotations.NotNull;
+import org.jivesoftware.smackx.Form;
+import org.jivesoftware.smackx.ReportedData;
+import org.jivesoftware.smackx.ReportedData.Row;
+import org.jivesoftware.smackx.search.UserSearch;
+import org.jivesoftware.smackx.search.UserSearchManager;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 class Main{
     static boolean login(@NotNull XMPPConnection con){
@@ -19,6 +24,53 @@ class Main{
     static void logout(@NotNull XMPPConnection con){
         con.disconnect();
     }
+    static boolean sendMessage(Chat chat, String JID, String message){
+        try {
+            chat.sendMessage(message);
+            return true;
+        }catch (Exception e){
+            System.out.println(e.toString());
+            return false;
+        }
+    }
+    static void getAllXmppUsers(XMPPConnection con){
+        try {
+            UserSearchManager manager = new UserSearchManager(con);
+            String searchFormString = "search." + con.getServiceName();
+            System.out.println("***" + "SearchForm: " + searchFormString);
+            Form searchForm = null;
+
+            searchForm = manager.getSearchForm(searchFormString);
+
+            Form answerForm = searchForm.createAnswerForm();
+
+            UserSearch userSearch = new UserSearch();
+            answerForm.setAnswer("Username", true);
+            answerForm.setAnswer("search", "*");
+            ReportedData results = userSearch.sendSearchForm(con, answerForm, searchFormString);
+            if (results != null) {
+                List<Row> actualList = new ArrayList<Row>();
+                results.getRows().forEachRemaining(actualList::add);
+                for (Row row: actualList ) {
+                    System.out.println("Usuario: " + row.getValues("JID").toString());
+                }
+            } else {
+                System.out.println("no result found");
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+    static void registerAccount(XMPPConnection connection){
+        AccountManager accountManager = new AccountManager(connection);
+        try{
+            accountManager.createAccount("rodrigoRemote2", "redes");
+            System.out.println("success");
+        }catch (XMPPException e1){
+            System.out.println(e1.toString());
+        }
+    }
 
     public static void main(String args[]){
         System.out.println("hola");
@@ -28,6 +80,7 @@ class Main{
             if (login(con)) {
                 ChatManager c = con.getChatManager();
                 Chat chat = c.createChat("echobot@alumchat.xyz", null);
+                registerAccount(con);
                 c.addChatListener(new ChatManagerListener() {
                     @Override
                     public void chatCreated(Chat chat, boolean create) {
@@ -40,7 +93,8 @@ class Main{
                         });
                     }
                 });
-                chat.sendMessage("Buenas noches");
+//                chat.sendMessage("Buenas noches");
+                System.out.println("done");
                 while (true) ;
             }else{
                 System.out.println("falló");
